7f02d21d38220b8ba44c36c5eab1d0b2f95b5fa9
Zeeshan Mustafa <zeeshan.mustafa91@gmail.com>
2025-04-26 15:41:42 +0500

    Configured MediatR to publish events in parallel, added another event.

--- CleanAPI.Application/DependencyInjection.cs
+++ CleanAPI.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MediatR.NotificationPublishers;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CleanAPI.Application;
 
@@ -7,7 +8,11 @@ public static class DependencyInjection
     public static IServiceCollection AddApplicationDI(this IServiceCollection services)
     {
 
-        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            cfg.NotificationPublisher = new TaskWhenAllPublisher(); 
+        });
 
         return services;
     }
---
+++ CleanAPI.Application/Events/StartMembershipEventHandler.cs
@@ -0,0 +1,29 @@
+﻿
+using CleanAPI.Application.Interfaces;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace CleanAPI.Application.Events;
+
+
+/// <summary>
+/// When Employee is created, this event is published.
+/// </summary>
+public class StartMembershipEventHandler : INotificationHandler<EmployeeCreatedEvent>
+{
+    private readonly ILogger<EmployeeCreatedEventHandler> _logger;
+    private readonly IEmailService _emailService;
+
+    public StartMembershipEventHandler(ILogger<EmployeeCreatedEventHandler> logger, IEmailService emailService)
+    {
+        _logger = logger;
+        _emailService = emailService;
+    }
+
+    public async Task Handle(EmployeeCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        await Task.Delay(2000);
+        _logger.LogInformation($"Membeship enabled for employee: {notification.Employee.Id} - {notification.Employee.FirstName} {notification.Employee.LastName}");
+
+    }
+}
