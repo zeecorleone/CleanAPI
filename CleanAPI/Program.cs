f8b88a672d17e5bc49b0e89a134a5e2c4172239f
Zeeshan Mustafa <zeeshan.mustafa91@gmail.com>
2025-04-30 22:43:58 +0500

    Added Azure App Insights for Monitoring

--- CleanAPI/Program.cs
+++ CleanAPI/Program.cs
@@ -1,8 +1,15 @@
 using CleanAPI;
+using Microsoft.ApplicationInsights.DependencyCollector;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddApplicationInsightsTelemetry();
+builder.Services.ConfigureTelemetryModule<DependencyTrackingTelemetryModule>((module, o) =>
+{
+    module.EnableSqlCommandTextInstrumentation = true;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
